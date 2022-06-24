ff0e098d9d76fb257975e027b3f9166fa08eed6a
Wafa-Mardiney <wafa@rayqube.com>
2022-06-24 09:35:51 +0400

    Fixed rotation of curves to follow rotation of the city

--- Assets/Scripts/CurvedLineRenderer.cs
+++ Assets/Scripts/CurvedLineRenderer.cs
@@ -1,98 +1,98 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
 
-[RequireComponent( typeof(LineRenderer) )]
-public class CurvedLineRenderer : MonoBehaviour 
+[RequireComponent(typeof(LineRenderer))]
+public class CurvedLineRenderer : MonoBehaviour
 {
-	//PUBLIC
-	public float lineSegmentSize = 0.15f;
-	public float lineWidth = 0.1f;
-	[Header("Gizmos")]
-	public bool showGizmos = true;
-	public float gizmoSize = 0.1f;
-	public Color gizmoColor = new Color(1,0,0,0.5f);
-	//PRIVATE
-	private CurvedLinePoint[] linePoints = new CurvedLinePoint[0];
-	private Vector3[] linePositions = new Vector3[0];
-	private Vector3[] linePositionsOld = new Vector3[0];
+    //PUBLIC
+    public float lineSegmentSize = 0.15f;
+    public float lineWidth = 0.1f;
+    [Header("Gizmos")]
+    public bool showGizmos = true;
+    public float gizmoSize = 0.1f;
+    public Color gizmoColor = new Color(1, 0, 0, 0.5f);
+    //PRIVATE
+    private CurvedLinePoint[] linePoints = new CurvedLinePoint[0];
+    private Vector3[] linePositions = new Vector3[0];
+    private Vector3[] linePositionsOld = new Vector3[0];
+    LineRenderer line;
+    Material material;
+    float offset;
+    Vector3[] smoothedPoints;
+    private void Awake()
+    {
+        line = this.GetComponent<LineRenderer>();
+        material = line.sharedMaterial;
 
-	// Update is called once per frame
-	public void Update () 
-	{
-		//GetPoints();
-		SetPointsToLine();
-		float offset = Time.time * -2;
-		LineRenderer line = this.GetComponent<LineRenderer>();
-		line.sharedMaterial.SetTextureOffset("_MainTex", new Vector2(offset, 0));
-		
-	}
 
-	void GetPoints()
-	{
-		//find curved points in children
-		linePoints = this.GetComponentsInChildren<CurvedLinePoint>();
+        //find curved points in children
+        linePoints = this.GetComponentsInChildren<CurvedLinePoint>();
+        linePositions = new Vector3[linePoints.Length];
+        linePositionsOld = new Vector3[linePositions.Length];
+        FillOldPositions();
+    }
+    // Update is called once per frame
+    public void Update()
+    {
+        GetPoints();
+        SetPointsToLine();
+        offset = Time.time * -2;
+        material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
 
-		//add positions
-		linePositions = new Vector3[linePoints.Length];
-		for( int i = 0; i < linePoints.Length; i++ )
-		{
-			linePositions[i] = linePoints[i].transform.position;
-		}
-	}
+    void GetPoints()
+    {
+        //add positions
+        for (int i = 0; i < linePoints.Length; i++)
+            linePositions[i] = linePoints[i].transform.position;
+    }
+    void FillOldPositions()
+    {
+        for (int i = 0; i < linePositions.Length; i++)
+            linePositionsOld[i] = linePositions[i];
+    }
+    void SetPointsToLine()
+    {
+        //check if line points have moved
+        bool moved = false;
+        for (int i = 0; i < linePositions.Length; i++)
+        {
+            //compare
+            if (linePositions[i] != linePositionsOld[i])
+                moved = true;
+        }
 
-	void SetPointsToLine()
-	{
-		//create old positions if they dont match
-		if( linePositionsOld.Length != linePositions.Length )
-		{
-			linePositionsOld = new Vector3[linePositions.Length];
-		}
+        //update if moved
+        if (moved == true)
+        {
+            //get smoothed values
+            smoothedPoints = LineSmoother.SmoothLine(linePositions, lineSegmentSize);
 
-		//check if line points have moved
-		bool moved = false;
-		for( int i = 0; i < linePositions.Length; i++ )
-		{
-			//compare
-			if( linePositions[i] != linePositionsOld[i] )
-			{
-				moved = true;
-			}
-		}
+            //set line settings
+            line.SetVertexCount(smoothedPoints.Length);
+            line.SetPositions(smoothedPoints);
+            line.SetWidth(lineWidth, lineWidth);
+            FillOldPositions();
+        }
+    }
 
-		//update if moved
-		if( moved == true )
-		{
-			LineRenderer line = this.GetComponent<LineRenderer>();
+    void OnDrawGizmosSelected()
+    {
+        Update();
+    }
 
-			//get smoothed values
-			Vector3[] smoothedPoints = LineSmoother.SmoothLine( linePositions, lineSegmentSize );
+    void OnDrawGizmos()
+    {
+        if (linePoints.Length == 0)
+        {
+            GetPoints();
+        }
 
-			//set line settings
-			line.SetVertexCount( smoothedPoints.Length );
-			line.SetPositions( smoothedPoints );
-			line.SetWidth( lineWidth, lineWidth );
-		}
-	}
-
-	void OnDrawGizmosSelected()
-	{
-		Update();
-	}
-
-	void OnDrawGizmos()
-	{
-		if( linePoints.Length == 0 )
-		{
-			GetPoints();
-		}
-
-		//settings for gizmos
-		foreach( CurvedLinePoint linePoint in linePoints )
-		{
-			linePoint.showGizmo = showGizmos;
-			linePoint.gizmoSize = gizmoSize;
-			linePoint.gizmoColor = gizmoColor;
-		}
-	}
+        //settings for gizmos
+        foreach (CurvedLinePoint linePoint in linePoints)
+        {
+            linePoint.showGizmo = showGizmos;
+            linePoint.gizmoSize = gizmoSize;
+            linePoint.gizmoColor = gizmoColor;
+        }
+    }
 }
--- Assets/Scripts/LineRestarter.cs
+++ Assets/Scripts/LineRestarter.cs
@@ -5,25 +5,11 @@ using UnityEngine;
 public class LineRestarter : MonoBehaviour
 {
     public CurvedLineRenderer[] allLines;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnEnable()
     {
         allLines = FindObjectsOfType<CurvedLineRenderer>();
         for (int i = 0; i < allLines.Length; i++)
-        {
             allLines[i].gameObject.SetActive(false);
-        }
         StartCoroutine(startLineAgain());
     }
 
